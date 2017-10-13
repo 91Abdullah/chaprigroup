db438693f4f7fbcc7e56cd0fd672395ca812a870
Abdullah Basit <abdullah.basit@hotmail.com>
2017-10-13 12:42:35 +0500

    removed async in scripts

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -90,8 +90,8 @@
     </script>
 
 
-    <script async src="https://www.youtube.com/iframe_api"></script>
-    <script async src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="https://www.youtube.com/iframe_api"></script>
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
     <script>
         window.jQuery || document.write('<script src="{{ asset(`js/jquery-1.10.2.min.js`) }}"><\/script>')
     </script>
